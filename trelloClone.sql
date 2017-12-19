892dab425ab191c9c509de90ea9646c27d0e9484
mranchon <dm.ranchon@gmail.com>
2017-12-19 10:08:54 +0100

    script sql

---
+++ trelloClone.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS appliWeb;
+CREATE TABLE utilisateur (
+  id int NOT NULL AUTO_INCREMENT,
+  identifiant varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE tache (
+  id int NOT NULL AUTO_INCREMENT,
+  titre varchar(255) NOT NULL,
+  statut varchar(255) DEFAULT 'A faire',
+  responsable varchar(255),
+  PRIMARY KEY (id)
+);
